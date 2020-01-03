6f6c9d8707e8d69d689844c7763d8af2981256a2
keisuke ohashi <kog71903@gmail.com>
2020-01-03 14:31:37 +0900

    [update]タスクリスト全体を動かせるように

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ const createTodoCard = (form) => {
     taskLevel = 'whenever';
   }
   const html = `
-  <li>
-    <p><span class="card ${taskLevel}">${form.category.value}</span>${form.task.value}</p>
+  <li class="card-lg">
+    <p><span class="card-sm ${taskLevel}">${form.category.value}</span>${form.task.value}</p>
   </li>
   `;
 
@@ -26,10 +26,32 @@ addTaskForm.addEventListener('submit', e => {
   e.preventDefault();
 
   // タスクに入力した値を空白を除外して格納
-
   if(addTaskForm.task.value.length > 0) {
       createTodoCard(addTaskForm);
       // タスクに入力した文字をクリア
       addTaskForm.reset();
   }
 });
+
+let isDraged = false;
+
+todos.onclick = function(event) {
+  if (isDraged === true) {
+    isDraged = false;
+  } else {
+    isDraged = true;
+  }
+}
+
+let onMouseMove = function(event){
+  if (isDraged === false) {
+    return;
+  }
+  let x = event.clientX;
+  let y = event.clientY;
+  let width = todos.offsetWidth;
+  let height = todos.offsetHeight;
+  todos.style.top = (y-height/2) + "px";
+  todos.style.left = (x-width/2) + "px";
+}
+todos.addEventListener("mousemove", onMouseMove);
